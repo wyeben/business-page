fbcc65bf05fe264a2a7e1f652ae8916603369320
Wyeben <yila.ben@yahoo.com>
2024-02-09 08:09:04 +0100

    added code

--- forms/contact.php
+++ forms/contact.php
@@ -13,7 +13,6 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];

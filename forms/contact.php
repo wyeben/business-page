989f5b2c13dfbe98d32444d479f83e088aab1c0a
Wyeben <yila.ben@yahoo.com>
2024-02-11 08:06:41 +0100

    added code

--- forms/contact.php
+++ forms/contact.php
@@ -27,7 +27,7 @@
   */
   $contact->add_message()
   $contact->add_message( $_POST['name'], 'From');
-  
+
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 

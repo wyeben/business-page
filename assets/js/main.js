d130b3318627c693e5f689b5dde702f664e242ed
Wyeben <yila.ben@yahoo.com>
2024-02-13 11:12:16 +0100

    added code

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,7 +188,6 @@
 
         portfolioIsotope.arrange({
           filter: this.getAttribute('data-filter')
-
         });
         portfolioIsotope.on('arrangeComplete', function() {
           AOS.refresh()

cc1e9de9dbec40c57220cf1ab7dae0d0eb4da641
Wyeben <yila.ben@yahoo.com>
2024-02-21 09:02:32 +0100

    added code

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,7 +182,7 @@
       on('click', '#portfolio-flters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
-  
+
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
